9d46a8b44461e800874d2f8ebf00986e0f953a71
nixoff <gabadev48@protonmail.com>
2021-07-13 17:11:23 -0300

    :tada: alurakut start :tada:

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,106 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid';
+import Box from '../src/components/Box';
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { ProfileRelationsBoxWrapper } from '../src/components/profileRelations';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSidebar(props) {
+  // console.log(props)
+
+  return (
+    <Box >
+      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }} />
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = 'nixoff';
+  const favoritePeoples = [
+    'juunegreiros',
+    'omariosouto',
+    'peas', 
+    'rafaballerini', 
+    'marcobrunodev',
+    'felipefialho'
+  ]
+
+
+  const usersFollowing = async() => {
+    try {
+      const response = await fetch('https://api.github.com/users/Nixoff/following')
+      const r = response.json();
+      console.log(r);
+      console.log(r.map(user => user.login))
+
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  usersFollowing()
+
+
+  function getUserFollowings() {
+    fetch('https://api.github.com/users/Nixoff/following')
+    .then((serverResponse) => {
+        return serverResponse.json();
+    })
+    .then((convertedUsers) => {
+        console.log(convertedUsers.map(user => user.login))
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() =>{
+        console.log('Passou no teste!')
+        let testando = convertedUsers.map(user => user.login);
+    })
+  
+  }
+  // getUserFollowings()
+
+  console.log('testando')
+  
+
+    return (
+      <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{  gridArea: 'profileArea' }}>
+          <ProfileSidebar githubUser={githubUser}/>
+        </div>
+
+        <div className="welcomeArea" style={{  gridArea: 'welcomeArea' }}>
+          <Box >
+            <h1 className="title">
+              Bem vindo(a) {githubUser}
+            </h1>
+
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+        
+        <div className="profileRelationsArea" style={{  gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+          Pessoas da comunidade ({favoritePeoples.length})
+            </h2>
+
+            <ul>
+            {favoritePeoples.map((currentItem) => {
+              return (
+                <li>
+                  <a href={`/users/${currentItem}`} key={currentItem}> 
+                <img src={`https://github.com/${currentItem}.png`}/>
+                <span>{currentItem}</span>
+                </a>
+                </li>
+              )
+            })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+    )
 }
